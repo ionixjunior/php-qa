a04218b39e596e2c66c835eee7a671463fc4b7ef
Ione Souza Junior <junior@ionixjunior.com.br>
2014-10-13 00:22:19 -0300

    PHPCS 2

--- src/Qualidade/Processos.php
+++ src/Qualidade/Processos.php
@@ -2,9 +2,13 @@
 
 namespace Qualidade;
 
-class Processos {
+class Processos
+{
 
-    public function processo1($variavel1, $variavel2) {
-        return $variavel1 * $variavel2;
+   private $id;
+   
+    public function __construct($id)
+    {
+        $this->id = $id;
     }
 }
