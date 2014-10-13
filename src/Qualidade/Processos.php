1698520eb15bb9bef7631d829945117b51e4b983
Ione Souza Junior <junior@ionixjunior.com.br>
2014-10-13 00:43:54 -0300

    PHPCPD 1

--- src/Qualidade/Processos.php
+++ src/Qualidade/Processos.php
@@ -2,17 +2,41 @@
 
 namespace Qualidade;
 
+use Exception;
+
 class Processos
 {
 
-    public function processo1($variavel1, $variavel2)
+    public function processo1($var1, $var2)
     {
-        return $variavel1 * $variavel2;
+        if (is_null($var1)) {
+            throw new Exception();
+        }
+        
+        if (is_null($var2)) {
+            throw new Exception();
+        }
+        
+        return $var1 + $var2;
     }
     
-    private function processo2()
+    public function processo2($var1, $var2, $var3)
     {
+        if (is_null($var1)) {
+            throw new Exception();
+        }
         
+        if (is_null($var2)) {
+            throw new Exception();
+        }
+        
+        if (   is_null($var3)
+            || $var3 === 0
+        ) {
+            throw new Exception();
+        }
+        
+        return ($var1 + $var2) / $var3;
     }
     
 }
