2e44e1852db3a1c6743e0454c2b8987e40553299
Ione Souza Junior <junior@ionixjunior.com.br>
2014-10-13 01:23:19 -0300

    PHPDOC 1

---
+++ src/Qualidade/ProcessosAdmin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Qualidade;
+
+use Qualidade\Processos;
+
+/**
+ * Classe de processos admin
+ */
+class ProcessosAdmin extends Processos
+{
+
+    /**
+     * Método de processo 1 admin.
+     * 
+     * @TODO futuramente implementar regra para validar processo.
+     */
+    public function processo1Admin()
+    {
+        
+    }
+    
+    /**
+     * Processo 2 admin.
+     * 
+     * @deprecated use método novoProcesso2Admin
+     */
+    public function processo2Admin()
+    {
+        
+    }
+    
+    /**
+     * Novo método de processo 2 admin.
+     */
+    public function novoProcesso2Admin()
+    {
+        
+    }
+}
